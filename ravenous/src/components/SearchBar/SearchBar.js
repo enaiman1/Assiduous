2f3ab28983238e7ed34e8d9b2724627d8a3a5a53
Ericcentrik <enaiman1@gmail.com>
2020-05-07 12:11:27 -0400

    added state to the search bar component

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-}
 
 class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      term: '' ,
+      location: '',
+      sortBy: 'best_match',
+
+    }
+    this.handleTermChange = this.handleTermChange.bind(this)
+    this.handleLocationChange = this.handleLocationChange.bind(this)
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    }
+  }
+  // this method will display what option the user selected (sorted from the sortByOptions object)
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption]
-      return <li key={sortByOptionValue}>{sortByOption}</li>
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption]
+      return <li 
+      className={this.getSortByClass(sortByOptionValue)} 
+      key={sortByOptionValue}
+      onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+      >
+        {sortByOption}
+        </li>
     });
   }
+
+  // this method will return the css to highlight the sorted option the user picked
+  getSortByClass(sortByOption){
+    if(this.state.sortBy === sortByOption){
+      return 'active'
+    } else {
+      return ''
+    }
+  }
+
+  // this method handles the state change when the user picks an option
+  handleSortByChange(sortByOption){
+    this.setState({sortBy: sortByOption})
+  }
+
+  // this method handles the changes when the user inputs values to the "search by business" search bar
+  handleTermChange(event){
+    this.setState({term: event.target.value})
+  }
+// this method handles the changes when the user inputs values to the "search by location" search bar
+  handleLocationChange(event){
+    this.setState({location: event.target.location})
+  }
+
 
   render() {
     return (
@@ -24,8 +66,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
           <a href="www.#.com">Let's Go</a>
