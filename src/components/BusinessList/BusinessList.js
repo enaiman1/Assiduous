6df2d3ada3be4a4e5038e3a4a2741fb56177cc90
Ericcentrik <enaiman1@gmail.com>
2020-05-07 16:25:39 -0400

    finished off front end

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-
+// this component is used to take the array of information and turn it to a new array to display on the page
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
                  {
                      this.props.businesses.map((business) => {
-                        return <Business business={business} />
+                        return <Business key={business.id} business={business} />
                      })
                  }
 
